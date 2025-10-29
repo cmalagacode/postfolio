ecd2229f69138fa5d7256d41c69731ac5a76b107
Cristian Malaga <cmalaga720@gmail.com>
2025-10-29 17:26:38 -0400

    Improve testing structure with better testing strategy. Enable testing database and seperate testing using ENV variable. Last of all, improve CI/CD.

--- schema/connection.py
+++ schema/connection.py
@@ -2,5 +2,10 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import create_async_engine
 
-ENGINE_SYNC = create_engine(os.environ["POSTFOLIO_DATABASE_URL"])
-ENGINE_ASYNC = create_async_engine(os.environ["POSTFOLIO_DATABASE_URL"].replace("postgresql://", "postgresql+asyncpg://"))
+if os.getenv("ENV", "testing") == "prod":
+    database_url = os.environ["POSTFOLIO_DATABASE_URL"]
+    ENGINE_SYNC = create_engine(database_url)
+    ENGINE_ASYNC = create_async_engine(database_url.replace("postgresql://", "postgresql+asyncpg://"))
+else:
+    ENGINE_SYNC = create_engine("sqlite:///file::memory:?cache=shared", connect_args={"check_same_thread": False, "uri": True})
+    ENGINE_ASYNC = ENGINE_SYNC
