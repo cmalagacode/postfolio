09b3ca7334e09fbffdeb9734094ea92758ecbfe7
Cristian Malaga <cmalaga720@gmail.com>
2025-11-07 01:43:56 -0500

    Implement feature posts.

--- model/user.py
+++ model/user.py
@@ -1,27 +1,24 @@
 from pydantic import BaseModel, EmailStr, Field, ConfigDict
 from model.settings import Timezones, PrivacySettings, Languages, ProfileTheme
-
-def to_camel(name: str) -> str:
-    parts = name.split("_")
-    return parts[0] + "".join(word.capitalize() for word in parts[1:])
+from model.util import to_camel
 
 class UserCreate(BaseModel):
     username: str = Field(min_length=7, max_length=30)
     email: EmailStr
     password: str = Field(min_length=8, max_length=70)
-    first_name: str = Field(min_length=1, max_length=50, alias="firstName")
-    last_name: str = Field(min_length=1, max_length=50, alias="lastName")
-    middle_name: str = Field(min_length=0, max_length=50, alias="middleName")
+    first_name: str = Field(min_length=1, max_length=100, alias="firstName")
+    last_name: str = Field(min_length=1, max_length=100, alias="lastName")
+    middle_name: str = Field(min_length=0, max_length=100, alias="middleName")
     timezone: Timezones = Field(default=Timezones.UTC)
 
 class GetUserResponse(BaseModel):
     bio: str = Field(min_length=0, max_length=200)
     email: EmailStr
-    first_name: str = Field(min_length=1, max_length=50)
+    first_name: str = Field(min_length=1, max_length=100)
     id: int
     is_active: bool
-    last_name: str = Field(min_length=1, max_length=50)
-    middle_name: str = Field(min_length=0, max_length=50)
+    last_name: str = Field(min_length=1, max_length=100)
+    middle_name: str = Field(min_length=0, max_length=100)
     privacy_settings: PrivacySettings
     profile_language: Languages
     profile_picture_url: str | None
--- schema/user.py
+++ schema/user.py
@@ -1,24 +1,22 @@
-from sqlalchemy import Integer, String, Boolean
-from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
+from sqlalchemy import Integer, String, Boolean, Text
+from sqlalchemy.orm import mapped_column, Mapped
 from sqlalchemy import Enum as SqlEnum
 from model.settings import PrivacySettings, ProfileTheme, Languages, Timezones
-
-class Base(DeclarativeBase):
-    pass
+from schema.base import Base
 
 class BlogUser(Base):
     __tablename__ = "blog_user"
-    bio: Mapped[str] = mapped_column(String, nullable=True, default="")
-    email: Mapped[str] = mapped_column(String, unique=True, index=True, nullable=False)
-    first_name: Mapped[str] = mapped_column(String, nullable=False)
+    bio: Mapped[str] = mapped_column(String(200), nullable=True, default="")
+    email: Mapped[str] = mapped_column(String(254), unique=True, index=True, nullable=False)
+    first_name: Mapped[str] = mapped_column(String(100), nullable=False)
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True, autoincrement=True, nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
-    last_name: Mapped[str] = mapped_column(String, nullable=False)
-    middle_name: Mapped[str] = mapped_column(String, nullable=False)
+    last_name: Mapped[str] = mapped_column(String(100), nullable=False)
+    middle_name: Mapped[str] = mapped_column(String(100), nullable=False)
     privacy_settings: Mapped[PrivacySettings] = mapped_column(SqlEnum(PrivacySettings, native_enum=False), default=PrivacySettings.PUBLIC, nullable=False)
     profile_language: Mapped[Languages] = mapped_column(SqlEnum(Languages, native_enum=False), default=Languages.ENGLISH, nullable=False)
-    profile_picture_url: Mapped[str | None] = mapped_column(String, nullable=True)
+    profile_picture_url: Mapped[str | None] = mapped_column(Text, nullable=True)
     profile_theme: Mapped[ProfileTheme] = mapped_column(SqlEnum(ProfileTheme, native_enum=False), default=ProfileTheme.SYSTEM_DEFAULT, nullable=False)
-    password: Mapped[str] = mapped_column(String, nullable=False)
+    password: Mapped[str] = mapped_column(String(60), nullable=False)
     timezone: Mapped[Timezones] = mapped_column(SqlEnum(Timezones, native_enum=False), default=Timezones.UTC, nullable=False)
-    username: Mapped[str] = mapped_column(String, unique=True, index=True, nullable=False)
+    username: Mapped[str] = mapped_column(String(30), unique=True, index=True, nullable=False)
